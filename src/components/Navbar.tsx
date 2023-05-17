db456271159058855016e7dbc91328e253a82f6f
rodrigof1307 <rodrigo.fernandes.1307@gmail.com>
2023-05-17 15:14:04 +0100

    Navbar Responsiveness

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,65 @@
 import Link from "next/link";
 import CustomConnectButton from "./CustomConnectButton";
+import * as Accordion from "@radix-ui/react-accordion";
+import { ChevronRight, Menu, X } from "lucide-react";
+import { cn } from "@/utils/utils";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 
-const Navbar = () => (
-  <nav className="relative z-20 flex flex-row items-center justify-between px-[4vw] py-[3vh] md:h-[7vw] md:py-0">
-    <ul className="flex flex-row justify-between text-[2.5vw] md:w-1/3 md:text-xl">
-      <li className="hover-line">
-        <Link href="/">Home</Link>
-      </li>
-      <li className="hover-line hidden md:block">
-        <Link href="/marketplace">Marketplace</Link>
-      </li>
-      <li className="hover-line hidden md:block">
-        <Link href="/my-collection">My collection</Link>
-      </li>
-    </ul>
-    <CustomConnectButton />
-  </nav>
+type NavbarLinksProps = {
+  className?: string;
+};
+
+const NavbarLinks = ({ className }: NavbarLinksProps) => (
+  <ul className={cn("flex w-full flex-col justify-between text-mb-3xl md:w-1/3 md:flex-row md:text-xl", className)}>
+    <li className="md:hover-line py-[1vh] md:block md:py-0">
+      <Link className="flex flex-row items-center justify-between" href="/">
+        Home
+        <ChevronRight className="block h-6 w-6 md:hidden" />
+      </Link>
+    </li>
+    <li className="md:hover-line py-[1vh] md:py-0">
+      <Link className="flex flex-row items-center justify-between" href="/marketplace">
+        Marketplace
+        <ChevronRight className="block h-6 w-6 md:hidden" />
+      </Link>
+    </li>
+    <li className="md:hover-line py-[1vh] md:py-0">
+      <Link className="flex flex-row items-center justify-between" href="/my-collection">
+        My Collection
+        <ChevronRight className="block h-6 w-6 md:hidden" />
+      </Link>
+    </li>
+  </ul>
 );
 
+const Navbar = () => {
+  const [value, setValue] = useState("");
+
+  const router = useRouter();
+
+  useEffect(() => {
+    setValue("");
+  }, [router.pathname]);
+
+  return (
+    <nav className="relative z-20 mb-[-1vh] px-[4vw] py-[3vh] md:mb-0 md:h-[7vw] md:py-[3vh]">
+      <Accordion.Root type="single" collapsible value={value} onValueChange={(val) => setValue(val)}>
+        <Accordion.Item value="item-1">
+          <div className="flex flex-row items-center justify-between">
+            <NavbarLinks className="hidden md:flex" />
+            <Accordion.Trigger className="group md:hidden">
+              <Menu className="flex h-6 w-6 group-data-[state=close]:flex group-data-[state=open]:hidden" aria-hidden />
+              <X className="hidden h-6 w-6 group-data-[state=open]:flex group-data-[state=close]:hidden" aria-hidden />
+            </Accordion.Trigger>
+            <CustomConnectButton />
+          </div>
+          <Accordion.Content className="overflow-hidden pt-[1vh] data-[state=closed]:animate-slideUp data-[state=open]:animate-slideDown md:hidden">
+            <NavbarLinks />
+          </Accordion.Content>
+        </Accordion.Item>
+      </Accordion.Root>
+    </nav>
+  );
+};
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -25,7 +25,7 @@ module.exports = {
       },
       fontSize: {
         xs: ["0.75vw", { lineHeight: "0.95vw" }],
-        sm: ["0.9vw", { lineHeight: "1.1vw" }],
+        sm: ["0.95vw", { lineHeight: "1.15vw" }],
         md: ["1.1vw", { lineHeight: "1.3vw" }],
         lg: ["1.3vw", { lineHeight: "1.5vw" }],
         xl: ["1.5vw", { lineHeight: "1.7vw" }],
@@ -34,7 +34,7 @@ module.exports = {
         "4xl": ["2.5vw", { lineHeight: "2.7vw" }],
         "5xl": ["3vw", { lineHeight: "3.2vw" }],
         "mb-xs": ["1.5vw", { lineHeight: "1.9vw" }],
-        "mb-sm": ["1.8vw", { lineHeight: "2.2vw" }],
+        "mb-sm": ["1.9vw", { lineHeight: "2.3vw" }],
         "mb-md": ["2.2vw", { lineHeight: "2.6vw" }],
         "mb-lg": ["2.6vw", { lineHeight: "3vw" }],
         "mb-xl": ["3vw", { lineHeight: "3.4vw" }],
@@ -44,6 +44,11 @@ module.exports = {
         "mb-5xl": ["6vw", { lineHeight: "6.4vw" }],
       },
       keyframes: {
+        opacityAnim: {
+          to: {
+            opacity: 1,
+          },
+        },
         overlayShow: {
           from: { opacity: 0 },
           to: { opacity: 1 },
@@ -52,10 +57,20 @@ module.exports = {
           from: { opacity: 0, transform: "translate(-50%, -48%) scale(0.96)" },
           to: { opacity: 1, transform: "translate(-50%, -50%) scale(1)" },
         },
+        slideDown: {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        slideUp: {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
       },
       animation: {
         overlayShow: "overlayShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
         contentShow: "contentShow 150ms cubic-bezier(0.16, 1, 0.3, 1)",
+        slideDown: "slideDown 300ms cubic-bezier(0.87, 0, 0.13, 1)",
+        slideUp: "slideUp 300ms cubic-bezier(0.87, 0, 0.13, 1)",
       },
     },
   },
